5ebcb0cf0a12dbc6f3a95958b3bd742361fa4ca6
Victor Lambret <lambret.victor@gmail.com>
2016-10-11 05:14:27 +0200

    [DEV] stack.sh: test argument number to avoid corrupting last source image if output is ommited

--- scripts/stack.sh
+++ scripts/stack.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function usage {
+	echo "stack.sh img1 img2 [...] output.png"
+	echo "        compose several images by stacking from background to foreground"
+}
+
+if [ 3 -gt $# ]; then
+	echo "illegal number of parameters"
+	usage
+	exit -1
+fi
+
 length=$(($#-3))
 array=(${@:3:$length})
 
