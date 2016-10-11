68b7019edc6e40266e83a1eeb296e216b4798535
Victor Lambret <lambret.victor@gmail.com>
2016-10-11 06:42:13 +0200

    [DEV] pos.sh : test parameters and add usage help

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-convert -size $4x$5 canvas:none -alpha transparent $6
-composite -geometry +$2+$3 $1 $6 $6
+function usage {
+	echo "pos.sh img x y width height output"
+	echo "        place an image at (x,y) offset in a (width x height) transparent image"
+}
+
+if [ 6 != $# ]; then
+	echo "Illegal number of parameters"
+	usage
+	exit -1
+fi
+
+SOURCE=$1
+X=$2
+Y=$3
+WIDTH=$4
+HEIGHT=$5
+OUTPUT=$6
+
+convert -size ${WIDTH}x${HEIGHT} canvas:none -alpha transparent $OUTPUT
+composite -geometry +${X}+${Y} $SOURCE $OUTPUT $OUTPUT
