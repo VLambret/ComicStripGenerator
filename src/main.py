bd8413ac269025908e9c6fa3f39e6ed139a87579
Albano Alice <alice.albano@albano_alice@yahoo.fr>
2017-03-26 00:45:15 +0100

    [DEV] big commit about ballooons !!!!!!
    Create rules for balloon generation in the makefile generator

--- src/main.py
+++ src/main.py
@@ -2,19 +2,23 @@ from Strip import *
 from PanelItem import *
 from Panel import *
 from MakefileGenerator import *
+from Balloon import *
 
 def main():
 	background = PanelItem("sources/background.png")
 	bluePerso = PanelItem("sources/perso1.png")
 	redPerso = PanelItem("sources/perso2.png")
 
-	panelTop = Panel(1074)
-	panelMiddle = Panel(1074)
-	panelBottom = Panel(1074)
+	balloon1 = Balloon(["Hi !"], 40, (-50, -35), (350, 100))
+
+	panelTop = Panel(1074, 768)
+	panelMiddle = Panel(1074, 768)
+	panelBottom = Panel(1074, 768)
 
 	panelTop.addPanelItem(background)
 	panelTop.addPanelItem(bluePerso)
 	panelTop.addPanelItem(redPerso)
+	panelTop.addBalloon(balloon1)
 
 	panelMiddle.addPanelItem(background)
 	panelMiddle.addPanelItem(redPerso)
