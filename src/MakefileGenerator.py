4da2dc27665c59221eff3c504858b0769c4ecfb8
Victor Lambret <lambret.victor@gmail.com>
2017-04-02 01:50:51 +0200

    fix last pylint errors by refactoring Makefile generation

--- src/MakefileGenerator.py
+++ src/MakefileGenerator.py
@@ -1,4 +1,4 @@
-from MakefileCommand import MakefileCommand
+from Makefile import Makefile
 
 class MakefileGenerator():
 
@@ -17,21 +17,17 @@ class MakefileGenerator():
             target_name = target_prefix + ".png"
             target_svg_name = target_prefix + ".svg"
 
-            pos_command = MakefileCommand.generate_position_command(balloon, panel)
+            pos_command = Makefile.generate_position_command(balloon, panel)
 
             svg_command = "./scripts/balloon.py -x 0 -y 0 -offset " + balloon.get_offset()
             svg_command += " -bx " + balloon.get_horizontal_orientation()
             svg_command += "  -by " + balloon.get_vertical_orientation()
             svg_command += " -c " + balloon.get_speech() + " > $@"
 
-            self.print_makefile_rule(target_pos_name, [target_name], [pos_command])
-            self.print_makefile_rule(target_svg_name, [""], [svg_command])
+            Makefile.print_makefile_rule(target_pos_name, [target_name], [pos_command])
+            Makefile.print_makefile_rule(target_svg_name, [""], [svg_command])
 
             balloon_counter += 1
-
-    def generate_pos_item_rule(self, target_name, panel_item, panel):
-        pos_command = MakefileCommand.generate_position_command(panel_item, panel)
-        self.print_makefile_rule(target_name, [panel_item.get_image_name()], [pos_command])
 
     def generate_panel_rules(self):
         panel_counter = 0
@@ -49,7 +45,7 @@ class MakefileGenerator():
                 item_pos_rule_name = target.replace(".png", "_pos" + str(item_counter) + ".png")
                 dependancies.append(item_pos_rule_name)
 
-                self.generate_pos_item_rule(item_pos_rule_name, panel_item, panel)
+                Makefile.generate_pos_item_rule(item_pos_rule_name, panel_item, panel)
 
                 item_counter += 1
 
@@ -60,7 +56,7 @@ class MakefileGenerator():
 
             self.generate_panel_balloon_rules(panel, panel_counter)
 
-            self.print_makefile_rule(target, dependancies, commands)
+            Makefile.print_makefile_rule(target, dependancies, commands)
             panel_counter += 1
 
     def generate_strip_rule(self):
@@ -70,33 +66,9 @@ class MakefileGenerator():
 
         commands = ["convert -append $^ $@"]
 
-        self.print_makefile_rule(self._png_target_name, dependancies, commands)
+        Makefile.print_makefile_rule(self._png_target_name, dependancies, commands)
 
     def generate_makefile(self):
-        self.print_generic_rules()
+        Makefile.print_generic_rules()
         self.generate_strip_rule()
         self.generate_panel_rules()
-
-    def print_makefile_rule(self, target, dependancies, commands):
-        print(target + " : ", end="")
-        dependancy_indentation = " " * (len(target) + 3)
-        first = True
-        dependancies_number = len(dependancies)
-        counter = 0
-        for dependancy in dependancies:
-            if first:
-                first = False
-            else:
-                print(dependancy_indentation, end="")
-            print(dependancy + " ", end="")
-
-            counter += 1
-            if counter != dependancies_number:
-                print(" \\")
-        print()
-        for command in commands:
-            print("\t" + command)
-        print()
-
-    def print_generic_rules(self):
-        self.print_makefile_rule("%.png", ["%.svg"], ["convert $< $@"])
