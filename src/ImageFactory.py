4c5c9cc88029e69fbab8b61ea6b8004ba8e41259
Victor Lambret <lambret.victor@gmail.com>
2017-04-08 20:19:45 +0200

    fix pylint issues on prototype

--- src/ImageFactory.py
+++ src/ImageFactory.py
@@ -2,21 +2,19 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-border = 40
+def draw_balloon(speech, border, font):
+    image = Image.new("RGB", (1, 1), "white")
+    draw = ImageDraw.Draw(image)
+    fontsize = 70
+    font = ImageFont.truetype(font, fontsize)
+    width, height = (draw.textsize(speech, font=font))
 
-def drawBalloon(speech):
-	im = Image.new("RGB", (1, 1), "white")
-	draw = ImageDraw.Draw(im)
-	fontsize = 70
-	font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf", fontsize)
-	x, y = (draw.textsize(speech, font=font))
+    size = (width + border, height + border)
+    image = Image.new("RGB", (size), "white")
+    draw = ImageDraw.Draw(image)
+    draw.rectangle([(0, 0), size], fill="white", outline="black")
+    draw.text((20, 20), speech, fill="black", font=font)
 
-	size = (x + border, y + border)
-	im = Image.new("RGB", (size), "white")
-	draw = ImageDraw.Draw(im)
-	draw.rectangle([(0, 0), size], fill="white", outline="black")
-	draw.text((20, 20), speech, fill="black", font = font)
+    image.show()
 
-	im.show()
-
-drawBalloon("Hello World!")
+draw_balloon("Hello World!", 40, "/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf")
