f8504885fed00d6eb3f2f0e5121bb0b39041b5cf
Victor Lambret <lambret.victor@gmail.com>
2017-04-08 13:27:25 +0200

    add a Strip class to handle strip properties

---
+++ src/Strip.py
@@ -0,0 +1,8 @@
+class Strip:
+
+    def __init__(self):
+        self.background_color = (255, 255, 255, 0)
+        self.panel_border_color = "black"
+        self.panel_border_size = 5
+        self.space_arround_panels = 25
+        self.panels = []
