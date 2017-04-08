75a3b19196da00ac9e34d1a78485511dc9581628
Victor Lambret <lambret.victor@gmail.com>
2017-04-08 12:19:58 +0200

    StripGenerator : overlay all panel items but balloons are missing

--- src/StripGenerator.py
+++ src/StripGenerator.py
@@ -9,24 +9,31 @@ def overlay_panel_items(image, panel_items):
         item_position = panel_item.get_position()
         item_image = Image.open(panel_item.get_image_name())
 
-        image.paste(item_image, item_position)
+        # PIL paste dont handle alpha, we have to use alpha_composite but it only
+        # accept images with the same size. The workarround here consist to create
+        # a temporary alpha image where the panel_item is pasted at the correct
+        # position and then composite this image.
+        alpha_tmp = Image.new('RGBA', image.size, (255, 255, 255, 0))
+        alpha_tmp.paste(item_image, item_position)
+        image = Image.alpha_composite(image, alpha_tmp)
+    return image
 
 def draw_borders(image):
     return ImageOps.expand(image, 5, "black")
 
 def create_image_from_panel(panel):
     panel_image = create_image_from_background(panel.get_background())
-    overlay_panel_items(panel_image, panel.get_panel_items())
+    panel_image = overlay_panel_items(panel_image, panel.get_panel_items())
     return draw_borders(panel_image)
 
-def create_image_from_strip(strip):
+def create_image_from_strip(strip, output_file_name):
     strip_width = 0
     strip_height = 0
     panel_image_list = []
     for panel in strip:
         panel_image = create_image_from_panel(panel)
         strip_width = max(strip_width, panel_image.size[0])
-        strip_height+= panel_image.size[1]
+        strip_height += panel_image.size[1]
         panel_image_list.append(panel_image)
 
     strip_image = Image.new("RGBA", (strip_width, strip_height), "white")
@@ -38,14 +45,4 @@ def create_image_from_strip(strip):
         strip_image.paste(panel_image, (0, height_offset))
         height_offset += panel_image.size[1]
 
-    strip_image.show()
-
-#perso = Image.new("RGB", (200, 400), "red")
-#realPerso = Image.open("sources/narrateur-smiling.png")
-#
-#background.paste(perso, (400,368))
-#background.paste(realPerso, (10,20))
-#background.paste(realPerso, (100,200))
-#background = ImageOps.expand(background, 5, "green")
-#
-#background.show()
+    strip_image.save(output_file_name, "PNG")
