39b9dc6912a597c39b805d1d864be1b237909850
Victor Lambret <lambret.victor@gmail.com>
2017-04-20 16:56:28 +0200

    StripParser : change tests order

--- src/Parser/StripParser_test.py
+++ src/Parser/StripParser_test.py
@@ -1,17 +1,5 @@
 import Parser.StripParser
 
-def test_empty_config():
-    result = Parser.StripParser.is_config("")
-    assert result is False, "Empty line"
-
-def test_valid_config():
-    result = Parser.StripParser.is_config("font_size:40")
-    assert result is True, "Should be valid"
-
-def test_unknown_config_key():
-    result = Parser.StripParser.is_background("unknown_key:strange_value")
-    assert result is False, "We should not accept unknown keys"
-
 def test_is_background():
     result = Parser.StripParser.is_background("")
     assert result is False, "Empty line"
@@ -33,6 +21,16 @@ def test_new_panel_from_background_line():
     panel = Parser.StripParser.new_panel_from_background_line("  =    background.png    ")
     background_name = panel.get_background().get_image_name()
     assert background_name == "sources/background.png", "valid background width spaces"
+
+def test_is_config():
+    result = Parser.StripParser.is_config("")
+    assert result is False, "Empty line"
+
+    result = Parser.StripParser.is_config("font_size:40")
+    assert result is True, "Should be valid"
+
+    result = Parser.StripParser.is_config("unknown_key:strange_value")
+    assert result is False, "We should not accept unknown keys"
 
 def test_empty_line():
     result = Parser.StripParser.is_empty_line("")
