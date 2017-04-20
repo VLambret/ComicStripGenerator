046dec94ffb73791c20cabd64c6357dab4e523ac
Victor Lambret <lambret.victor@gmail.com>
2017-04-20 15:33:47 +0200

    replace unittest by pytest

--- src/Parser/StripParser_test.py
+++ src/Parser/StripParser_test.py
@@ -1,19 +1,13 @@
-import unittest
 import Parser.StripParser
 
-class StripParserTestCase(unittest.TestCase):
+def test_empty_line():
+    result = Parser.StripParser.is_config("")
+    assert result is False, "Empty line"
 
-    def test_empty_line(self):
-        result = Parser.StripParser.is_config("")
-        assert result is False, "Empty line"
+def test_valid_config():
+    result = Parser.StripParser.is_config("font_size:40")
+    assert result is True, "Should be valid"
 
-    def test_valid_config(self):
-        result = Parser.StripParser.is_config("font_size:40")
-        assert result is True, "Should be valid"
-
-    def test_unknown_config_key(self):
-        result = Parser.StripParser.is_config("unknown_key:strange_value")
-        assert result is False, "We should not accept unknown keys"
-
-if __name__ == "__main__":
-    unittest.main()
+def test_unknown_config_key():
+    result = Parser.StripParser.is_config("unknown_key:strange_value")
+    assert result is False, "We should not accept unknown keys"
