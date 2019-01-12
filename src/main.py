72d09194405ab5d74dfe82980afb54f986e1759b
Victor Lambret <lambret.victor@gmail.com>
2019-01-12 13:04:28 +0100

    Make some integration tests pass

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 
+import sys
+
 import argparse
 import Parser.StripParser
 import StripGenerator
@@ -11,10 +13,32 @@ def get_arguments():
     args = parser.parse_args()
     return args.f
 
+def print_usage(command):
+    print("usage : " + command + " input.strip [output.strip]")
+
 def main():
-    comic_file_name = get_arguments()
-    final_png_name = comic_file_name.replace(".comic", ".png")
-    strip = Parser.StripParser.init_from_file(comic_file_name)
-    StripGenerator.create_image_from_strip(strip, final_png_name)
+    argc = len(sys.argv)
+    if (argc < 2 or 3 < argc):
+        exit(1)
+
+    if sys.argv[1] == "--help":
+        print_usage(sys.argv[0])
+        exit(0)
+
+    inputFile = sys.argv[1]
+    if argc == 3:
+        outputFile = sys.argv[2]
+    else:
+        outputFile = inputFile.replace(".strip", ".png")
+
+    inputStrip = open(inputFile,'r')
+
+    exit(0)
+
+
+    #comic_file_name = get_arguments()
+    #final_png_name = comic_file_name.replace(".comic", ".png")
+    #strip = Parser.StripParser.init_from_file(comic_file_name)
+    #StripGenerator.create_image_from_strip(strip, final_png_name)
 
 main()
--- test/integration/test.bats.sh
+++ test/integration/test.bats.sh
@@ -1,59 +1,56 @@
 #!/usr/bin/env bats
 
-CSG_COMMAND=../src/main.py
+CSG_COMMAND=../../src/main.py
 
 # Failure tests
 
 @test "Given no parameters command fails" {
-	run $COMMAND > /dev/null
+	run $CSG_COMMAND
 	[ $status -ne 0 ]
 	[ ! -f .png ]
 }
 
 @test "Given more than two parameters command fails" {
-	run $COMMAND one too many> /dev/null
+	run $CSG_COMMAND one too many> /dev/null
 	[ $status -ne 0 ]
 	[ ! -f one.png ]
 }
 
 @test "Given a non existing input file name command fails" {
-	run $COMMAND dontexist.strip > /dev/null
+	run $CSG_COMMAND dontexist.strip > /dev/null
 	[ $status -ne 0 ]
 	[ ! -f dontexist.png ]
 }
 
 @test "Given an empty input file command fails" {
-	run $COMMAND empty.strip > /dev/null
+	run $CSG_COMMAND empty.strip > /dev/null
 	[ $status -ne 0 ]
 	[ ! -f empty.png ]
 }
 
 @test "Given an invalid input file command fails" {
-	run $COMMAND invalid.strip > /dev/null
+	run $CSG_COMMAND invalid.strip > /dev/null
 	[ $status -ne 0 ]
 	[ ! -f invalid.png ]
 }
 
-# Valid usage tests
-
-@test "testing file existence" {
-}
+## Valid usage tests
 
 @test "Passing --help option print program usage on standard output" {
-	run $COMMAND --help > /dev/null
+	run $CSG_COMMAND --help > /dev/null
 	[ $status -eq 0 ]
 	[ $output != "" ]
 }
 
 @test "Given a valid input file name command produce an output with default name" {
-	run $COMMAND valid.strip > /dev/null
+	run $CSG_COMMAND valid.strip > /dev/null
 	[ $status -eq 0 ]
 	[ $output = "" ]
 	[ -f valid.png ]
 }
 
 @test "Given a valid input file name and a target name command produce an output with target name" {
-	run $COMMAND valid.strip valid_alternate.png > /dev/null
+	run $CSG_COMMAND valid.strip valid_alternate.png > /dev/null
 	[ $status -eq 0 ]
 	[ $output = "" ]
 	[ -f valid_alternate.png ]
