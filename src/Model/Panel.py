95a70a80959858aa0707f7d55470144e6247ec6b
Victor Lambret <lambret.victor@gmail.com>
2019-01-20 13:49:03 +0100

    Remove panel items from Panel

--- src/Model/Panel.py
+++ src/Model/Panel.py
@@ -2,7 +2,6 @@ class Panel:
 
     def __init__(self, background_panel_item):
         self.background = background_panel_item
-        self.panel_items = [background_panel_item]
         self.characters = []
         self.balloons = []
 
@@ -10,14 +9,11 @@ class Panel:
     def size(self):
         return self.background.get_size()
 
-    def add_panel_item(self, panel_item):
-        self.panel_items.append(panel_item)
-
     def add_balloon(self, balloon):
         self.balloons.append(balloon)
 
     def get_panel_items(self):
-        return self.characters + self.panel_items[1:]
+        return self.characters
 
     def add_character(self, character):
         self.characters.append(character)
