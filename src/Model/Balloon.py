4db11581348ca684f19832746379bc4fab66687c
Victor Lambret <lambret.victor@gmail.com>
2019-01-20 23:27:57 +0100

    Remove tail attributes from Balloon class

--- src/Model/Balloon.py
+++ src/Model/Balloon.py
@@ -8,8 +8,6 @@ class Balloon:
     def __init__(self, speech, position):
         self.image = ImageFactory.create_balloon_image_from(speech)
         self._position = position
-        self.tail_angle = 90
-        self.tail_length = 20
         self.speech = speech
 
     @property
@@ -23,7 +21,9 @@ class Balloon:
         return (position[0] + self.size[0] / 2, position[1] + self.size[1] - Config.border_width / 2)
 
     def get_tail_end_at(self, position):
+        tail_angle = 90
+        tail_length = 20
         start = self.get_tail_start_at(position)
-        offsetx = self.tail_length * math.cos(math.radians(self.tail_angle))
-        offsety = self.tail_length * math.sin(math.radians(self.tail_angle))
+        offsetx = tail_length * math.cos(math.radians(tail_angle))
+        offsety = tail_length * math.sin(math.radians(tail_angle))
         return (start[0] + offsetx, start[1] + offsety)
