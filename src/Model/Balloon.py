7a228c2056a152b8e3d4fac246a9edd40293c393
Victor Lambret <lambret.victor@gmail.com>
2019-01-24 09:01:41 +0100

    Balloon : make the class inherit from PanelItem

--- src/Model/Balloon.py
+++ src/Model/Balloon.py
@@ -1,20 +1,14 @@
 import Config
 import ImageFactory
+from Model.PanelItem import PanelItem
 
-class Balloon:
+class Balloon(PanelItem):
 
     def __init__(self, speech, position, target):
         self.image = ImageFactory.create_balloon_image_from(speech)
         self._position = position
         self._target = target
         self.speech = speech
-
-    @property
-    def size(self):
-        return self.image.size
-
-    def get_absolute_position_in(self, box):
-        return self._position.get_position_in(self.size, box)
 
     def get_tail_start_at(self, position):
         return (position[0] + self.size[0] / 2, position[1] + self.size[1] - Config.border_width / 2)
