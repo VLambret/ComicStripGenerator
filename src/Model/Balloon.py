47f01ca62930ff4e5e8740265c9953eaae7bacf8
Victor Lambret <lambret.victor@gmail.com>
2019-02-05 18:51:31 +0100

    Make Balloon rely more on super class constructor

--- src/Model/Balloon.py
+++ src/Model/Balloon.py
@@ -5,10 +5,9 @@ from Model.Item import Item
 class Balloon(Item):
 
     def __init__(self, speech, position, target):
-        self._image = ImageFactory.create_balloon_image_from(speech)
-        self._position = position
+        image = ImageFactory.create_balloon_image_from(speech)
+        super().__init__(image, position)
         self._target = target
-        self.speech = speech
 
     def get_tail_start_at(self, position):
         return (position[0] + self.size[0] / 2, position[1] + self.size[1] - Config.border_width / 2)
