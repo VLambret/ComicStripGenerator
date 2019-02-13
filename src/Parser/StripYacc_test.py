62667d566ead08cae7e93be351f7018be658bf39
Victor Lambret <lambret.victor@gmail.com>
2019-02-13 19:07:20 +0100

    Parser : Use a Factory pattern to generate strip source patterns

--- src/Parser/StripYacc_test.py
+++ src/Parser/StripYacc_test.py
@@ -6,6 +6,26 @@ import Config
 from Model.Strip import Strip
 from Parser.StripYacc import parser
 
+class Source():
+
+    def __init__(self):
+        self.lines = []
+        self.database = ""
+
+    def addPanel(self):
+        if not self.database:
+            self.with_database("../../sources")
+        self.lines.append("@background.png")
+        return self
+
+    def with_database(self, database):
+        self.database = 'database:' + database
+        return self
+
+    def build(self):
+        content = "\n".join([self.database] + self.lines)
+        print(content)
+        return io.StringIO(content)
 
 def test_parsing_an_empty_files_gives_an_empty_strip():
     input = io.StringIO("")
@@ -30,7 +50,7 @@ def test_multiline_comment():
     ("database", "../../sources"),
 ])
 def test_a_key_value_separated_with_a_colon_is_a_config_element(key, value):
-    input = io.StringIO(key + ":" + value)
+    input = Source().with_database(value).build()
     strip = parser.parse(input.read())
     assert isinstance(strip, Strip)
     assert len(strip.panels) == 0
