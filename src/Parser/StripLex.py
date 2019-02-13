702693ebacb2b2e0450754f91016049d5dffa31b
Victor Lambret <lambret.victor@gmail.com>
2019-02-13 09:26:30 +0100

    Parsing : Fix some parsing issues on how tokens are split

--- src/Parser/StripLex.py
+++ src/Parser/StripLex.py
@@ -1,31 +1,42 @@
 import ply.lex as lex
 
 tokens = (
+    'COMMENT',
     'UNKNOWN_LINE',
+    'VALUE',
     'KEY',
     'COLON',
-    'VALUE',
     'EOL',
 )
 
+def t_COMMENT(t):
+    r"""^\#[^\n]*"""
+    print(t)
+
 def t_KEY(t):
-    r"""database"""
+    #r"""database"""
+    r"""^[^:]+"""
+    print(t)
     return t
 
 def t_COLON(t):
     r""":"""
+    print(t)
     return t
 
 def t_VALUE(t):
-    r""".+"""
+    r"""[^:\n]+$"""
+    print(t)
     return t
 
 def t_UNKNOWN_LINE(t):
     r"[^\n]+"
+    print(t)
     return t
 
 def t_EOL(t):
     r"\n"
+    print(t)
     return t
 
 lexer = lex.lex()
--- src/Parser/StripYacc.py
+++ src/Parser/StripYacc.py
@@ -15,13 +15,13 @@ def p_lines(p):
 
 def p_line(p):
     """line : config_line
-            | unknown_line
-            | empty"""
+            | unknown_line"""
 
 def p_config_line(p):
     """config_line : KEY COLON VALUE"""
     print("key=", p[1], " detected ! with value=", p[3])
-    Config.image_database = p[3]
+    if p[1] == "database":
+        Config.image_database = p[3]
 
 def p_unknown_line(p):
     """unknown_line : UNKNOWN_LINE"""
@@ -30,6 +30,15 @@ def p_unknown_line(p):
 def p_empty(p):
     """empty : """
     print("Empty !")
+
+def p_error(p):
+    if p:
+        print("Syntax error at token", p.type, "with value=", p.value)
+        print("p=", p)
+        # Just discard the token and tell the parser it's okay.
+        parser.errok()
+    else:
+        print("Syntax error at EOF")
 
 parser = yacc.yacc(debug=True)
 
